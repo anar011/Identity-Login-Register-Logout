86d8f745f541e6d34d6f75d1e48aec327d11f8fc
anaraa@code.edu.az <you@example.com>
2023-05-02 14:18:02 -0700

    Login

---
+++ EntityFramework-Slider/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using EntityFramework_Slider.ViewModels.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntityFramework_Slider.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+      public IActionResult Register()
+      {
+            return View();
+      }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ EntityFramework-Slider/ViewModels/Account/RegisterVM.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityFramework_Slider.ViewModels.Account
+{
+    public class RegisterVM
+    {
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress, ErrorMessage = "E-mail is not valid")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password),Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
